5be16ad6e4de249d40aeb132a5851d410a21006c
cH000k <noelcedrix@yahoo.com>
2022-10-13 18:28:19 +0800

    10132022

--- nodejs/ejs/index.js
+++ nodejs/ejs/index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const { fileURLToPath } = require('url');
+const redditData = require('./data.json');
+console.log(redditData);
 
 // Use EJS. No need to require the package
 app.set('view engine', 'ejs');
@@ -24,7 +26,15 @@ app.get('/rand', (req, res) => {
 // Passing data from params to templates
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit });
+    const data = redditData[subreddit];
+
+    // with error handling
+    if (data) {
+        res.render('subreddit', { subreddit, ...data });
+    } else {
+        res.render('notfound', { subreddit });
+    }
+    
 })
 
 // Loop in templats
