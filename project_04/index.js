9dcc0c694228b0849f4c1d6f340a6ab9b0ce1804
cH000k <noelcedrix@yahoo.com>
2022-10-16 19:11:05 +0800

    10162022 simple page with routing and ejs

--- project_04/index.js
+++ project_04/index.js
@@ -6,8 +6,22 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(exrpess.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/services', (req, res) => {
+    res.render('services');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
 })
 
 app.listen(port, () => {
---
+++ project_04/public/js/app.js
@@ -0,0 +1,22 @@
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const services = document.querySelector('#services');
+const contact = document.querySelector('#contact');
+
+home.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+about.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+services.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+contact.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+
